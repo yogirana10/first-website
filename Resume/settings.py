b59b904dbf81745999867bf202e55fb957c36afa
prashant_rana <rana5@umbc.edu>
2018-11-23 20:34:58 -0500

    My website Fourth commit

--- Resume/settings.py
+++ Resume/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('/etc/secret_key.txt') as f:
+with open('D:/Resume_Website/secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
